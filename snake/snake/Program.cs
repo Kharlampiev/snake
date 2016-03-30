56b8726e4c886591106a649afcc44a46a6caeb61
Kharlampiev <lippogoo@gmail.com>
2016-03-30 17:18:21 +0300

    Set length snake

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -23,7 +23,7 @@ namespace snake
 
 
             Point p = new Point(4,5,'*');
-            p.Draw();
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
                       
             Console.ReadLine();
         }
